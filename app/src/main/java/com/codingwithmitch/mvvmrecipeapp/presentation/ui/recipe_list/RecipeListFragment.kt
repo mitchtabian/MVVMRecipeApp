fe1459f21504cb985baac3e69f0bd6bc34455c78
mitch <mitch@tabian.ca>
2020-12-03 12:18:53 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -94,7 +94,7 @@ class RecipeListFragment: Fragment() {
 
                 AppTheme(
                         darkTheme = !application.isLight,
-                        progressBarIsDisplayed = false,
+                        progressBarIsDisplayed = displayProgressBar,
                 ){
                     val scaffoldState = rememberScaffoldState()
 
