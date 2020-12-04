56caef12de15cdc09c9785c294b8964a7acc764a
mitch <mitch@tabian.ca>
2020-12-04 10:36:58 -0800

    Column layouts

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
@@ -2,31 +2,83 @@ package com.codingwithmitch.mvvmrecipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.imageFromResource
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity(){
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Column(
-                    modifier = Modifier.padding(16.dp)
+            ScrollableColumn(
+                    modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .background(color = Color(0xFFF2F2F2))
             ) {
-                Text(text = "Hey look some text")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = {}) {
-                    Text(text = "A BUTTON")
+                Image(
+                        bitmap = imageFromResource(res = resources, resId = R.drawable.happy_meal),
+                        modifier = Modifier.height(300.dp),
+                        contentScale = ContentScale.Crop,
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                            text = "Happy Meal",
+                            style = TextStyle(
+                                    fontSize = TextUnit.Companion.Sp(26)
+                            )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 8.dp))
+                    Text(
+                            text = "800 calories",
+                            style = TextStyle(
+                                    fontSize = TextUnit.Companion.Sp(17)
+                            )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 8.dp))
+                    Text(
+                            text = "$5.99",
+                            style = TextStyle(
+                                    color = Color(0xFF85bb65),
+                                    fontSize = TextUnit.Companion.Sp(17)
+                            )
+                    )
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
