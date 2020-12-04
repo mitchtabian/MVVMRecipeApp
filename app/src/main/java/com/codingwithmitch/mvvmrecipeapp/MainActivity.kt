3ab5c9cd745dc9ae4dd2f559ad86adde72efac39
mitch <mitch@tabian.ca>
2020-12-04 10:05:04 -0800

    jetpack compose setup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
@@ -2,6 +2,14 @@ package com.codingwithmitch.mvvmrecipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity(){
 
@@ -9,5 +17,16 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            Column(
+                    modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "A BUTTON")
+                }
+            }
+        }
     }
 }
