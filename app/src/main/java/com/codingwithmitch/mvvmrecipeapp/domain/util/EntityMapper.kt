0c539995321d92369920619b5006a943ae8c442e
mitch <mitch@tabian.ca>
2020-12-07 15:58:03 -0800

    Add NetworkEntity mapper

---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/domain/util/EntityMapper.kt
@@ -0,0 +1,8 @@
+package com.codingwithmitch.mvvmrecipeapp.domain.util
+
+interface EntityMapper <Entity, DomainModel>{
+
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
