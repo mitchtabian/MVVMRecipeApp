3a78250ce37168f6b3de4a75c3a81b5bc5d3816b
mitch <mitch@tabian.ca>
2020-12-07 14:05:04 -0800

    build domain model

---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/domain/Recipe.kt
@@ -0,0 +1,22 @@
+package com.codingwithmitch.mvvmrecipeapp.domain
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * See Recipe example: https://food2fork.ca/
+ */
+@Parcelize
+class Recipe (
+    var id: Int? = null,
+    var title: String? = null,
+    var publisher: String? = null,
+    var featuredImage: String? = null,
+    var rating: Int? = 0,
+    var sourceUrl: String? = null,
+    var description: String? = null,
+    var cookingInstructions: String? = null,
+    var ingredients: List<String>? = null,
+    var dateAdded: String? = null,
+    var dateUpdated: String? = null,
+): Parcelable
