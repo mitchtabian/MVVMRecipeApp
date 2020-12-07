5394e418d7cff17c153b65cd44f3d81ec268c1a0
mitch <mitch@tabian.ca>
2020-12-07 10:24:03 -0800

    Add ComposeView

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
@@ -2,71 +2,13 @@ package com.codingwithmitch.mvvmrecipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccessTime
-import androidx.compose.material.icons.filled.Adb
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent{
-            ListItem(
-                    modifier = Modifier
-                            .height(100.dp)
-                            .border(border = BorderStroke(1.dp, Color.Black))
-                    ,
-                    icon = {
-                        Icon(
-                                Icons.Filled.Adb,
-                                modifier = Modifier
-                                        .border(border = BorderStroke(1.dp, Color.Black))
-                                )
-                    },
-                    trailing = {
-                        IconButton(
-                                onClick = {},
-                                modifier = Modifier
-                                        .border(border = BorderStroke(1.dp, Color.Black))
-                        ) {
-                            Icon(Icons.Filled.AccessTime)
-                        }
 
-                    },
-                    overlineText = {
-                        Text(
-                                text = "Overline text",
-                                modifier = Modifier
-                                        .border(border = BorderStroke(1.dp, Color.Black))
-                        )
-                    },
-                    secondaryText = {
-                        Text(
-                                text = "Secondary text",
-                                modifier = Modifier
-                                        .border(border = BorderStroke(1.dp, Color.Black))
-                        )
-                    }
-            ) {
-                Text(
-                        "Big Title Text",
-                        modifier = Modifier
-                                .border(border = BorderStroke(1.dp, Color.Black))
-                )
-            }
-        }
     }
 }
 
---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/RecipeListFragment.kt
@@ -0,0 +1,20 @@
+package com.codingwithmitch.mvvmrecipeapp
+
+import androidx.fragment.app.Fragment
+
+class RecipeListFragment: Fragment() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
