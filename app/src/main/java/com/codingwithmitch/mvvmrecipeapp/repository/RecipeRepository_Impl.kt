a0c11970bec086d6642df88dfd9e2ada76cfc52e
mitch <mitch@tabian.ca>
2020-12-14 13:11:57 -0800

    Change mapping terminology

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/repository/RecipeRepository_Impl.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/repository/RecipeRepository_Impl.kt
@@ -10,7 +10,7 @@ class RecipeRepository_Impl (
 ): RecipeRepository {
 
     override suspend fun search(token: String, page: Int, query: String): List<Recipe> {
-        return mapper.fromEntityList(recipeService.search(token = token, page = page, query = query).recipes)
+        return mapper.toDomainList(recipeService.search(token = token, page = page, query = query).recipes)
     }
 
     override suspend fun get(token: String, id: Int): Recipe {
