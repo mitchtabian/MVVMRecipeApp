562cb854106656159b0bb8e3bf5ecb77ec5efe44
mitch <mitch@tabian.ca>
2020-12-14 15:46:59 -0800

    TODO: Fix elevation..?

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -44,41 +44,44 @@ class RecipeListFragment: Fragment() {
 
                 Column {
                     Surface(
+                            modifier = Modifier
+                                    .fillMaxWidth()
+                            ,
                             color = MaterialTheme.colors.secondary,
                             elevation = 8.dp,
-                    ) {
-                        Row(modifier = Modifier.fillMaxWidth()){
-                            TextField(
-                                    modifier = Modifier
-                                            .fillMaxWidth(.9f)
-                                            .padding(8.dp)
-                                    ,
-                                    value = query,
-                                    onValueChange = {
-                                        viewModel.onQueryChanged(it)
-                                    },
-                                    label = {
-                                        Text(text = "Search")
-                                    },
-                                    keyboardOptions = KeyboardOptions(
-                                            keyboardType = KeyboardType.Text,
-                                            imeAction = ImeAction.Done,
-                                    ),
-                                    leadingIcon = {
-                                        Icon(Icons.Filled.Search)
-                                    },
-                                    onImeActionPerformed = { action, softKeyboardController ->
-                                        if (action == ImeAction.Done) {
-                                            viewModel.newSearch(query)
-                                            softKeyboardController?.hideSoftwareKeyboard()
-                                        }
-                                    },
-                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                    backgroundColor = MaterialTheme.colors.surface
-                            )
-                        }
-                    }
-
+                            content = {
+                                Row(modifier = Modifier.fillMaxWidth()){
+                                    TextField(
+                                            modifier = Modifier
+                                                    .fillMaxWidth(.9f)
+                                                    .padding(8.dp)
+                                            ,
+                                            value = query,
+                                            onValueChange = {
+                                                viewModel.onQueryChanged(it)
+                                            },
+                                            label = {
+                                                Text(text = "Search")
+                                            },
+                                            keyboardOptions = KeyboardOptions(
+                                                    keyboardType = KeyboardType.Text,
+                                                    imeAction = ImeAction.Done,
+                                            ),
+                                            leadingIcon = {
+                                                Icon(Icons.Filled.Search)
+                                            },
+                                            onImeActionPerformed = { action, softKeyboardController ->
+                                                if (action == ImeAction.Done) {
+                                                    viewModel.newSearch(query)
+                                                    softKeyboardController?.hideSoftwareKeyboard()
+                                                }
+                                            },
+                                            textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                            backgroundColor = MaterialTheme.colors.surface
+                                    )
+                                }
+                            }
+                    )
                     LazyColumnForIndexed(
                             items = recipes
                     ) { index, recipe ->
