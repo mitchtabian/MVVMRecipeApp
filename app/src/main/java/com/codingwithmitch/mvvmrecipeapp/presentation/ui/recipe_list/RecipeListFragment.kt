d1cc7f073397a174ede5bd8527fa1780635506c8
mitch <mitch@tabian.ca>
2020-12-14 14:11:04 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,25 +1,14 @@
 package com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnForIndexed
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.RecipeCard
-import com.codingwithmitch.mvvmrecipeapp.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
