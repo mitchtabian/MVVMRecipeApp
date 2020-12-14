81370ea4c9dcc339d289d1f99e780aeba758f71b
mitch <mitch@tabian.ca>
2020-12-14 09:47:52 -0800

    Change mapping terminology

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/domain/util/EntityMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.codingwithmitch.mvvmrecipeapp.domain.util
-
-interface EntityMapper <Entity, DomainModel>{
-
-    fun mapFromEntity(entity: Entity): DomainModel
-
-    fun mapToEntity(domainModel: DomainModel): Entity
-}
---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/network/model/RecipeDtoMapper.kt
@@ -0,0 +1,50 @@
+package com.codingwithmitch.mvvmrecipeapp.network.model
+
+import com.codingwithmitch.mvvmrecipeapp.domain.model.Recipe
+import com.codingwithmitch.mvvmrecipeapp.domain.util.DomainMapper
+
+
+class RecipeDtoMapper : DomainMapper<RecipeDto, Recipe> {
+
+    override fun mapToDomainModel(model: RecipeDto): Recipe {
+        return Recipe(
+            id = model.pk,
+            title = model.title,
+            featuredImage = model.featuredImage,
+            rating = model.rating,
+            publisher = model.publisher,
+            sourceUrl = model.sourceUrl,
+            description = model.description,
+            cookingInstructions = model.cookingInstructions,
+            ingredients = model.ingredients.orEmpty(),
+            dateAdded = model.dateAdded,
+            dateUpdated = model.dateUpdated,
+        )
+    }
+
+    override fun mapFromDomainModel(domainModel: Recipe): RecipeDto {
+        return RecipeDto(
+            pk = domainModel.id,
+            title = domainModel.title,
+            featuredImage = domainModel.featuredImage,
+            rating = domainModel.rating,
+            publisher = domainModel.publisher,
+            sourceUrl = domainModel.sourceUrl,
+            description = domainModel.description,
+            cookingInstructions = domainModel.cookingInstructions,
+            ingredients = domainModel.ingredients,
+            dateAdded = domainModel.dateAdded,
+            dateUpdated = domainModel.dateUpdated,
+        )
+    }
+
+    fun fromEntityList(initial: List<RecipeDto>): List<Recipe>{
+        return initial.map { mapToDomainModel(it) }
+    }
+
+    fun toEntityList(initial: List<Recipe>): List<RecipeDto>{
+        return initial.map { mapFromDomainModel(it) }
+    }
+
+
+}
