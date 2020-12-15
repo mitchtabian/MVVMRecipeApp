0f2068667d3152db79acc0866c7884d78e215e8b
mitch <mitch@tabian.ca>
2020-12-14 16:24:29 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnForIndexed
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
@@ -47,41 +48,41 @@ class RecipeListFragment: Fragment() {
                             modifier = Modifier
                                     .fillMaxWidth()
                             ,
-                            color = MaterialTheme.colors.secondary,
+                            color = MaterialTheme.colors.primary,
                             elevation = 8.dp,
-                            content = {
-                                Row(modifier = Modifier.fillMaxWidth()){
-                                    TextField(
-                                            modifier = Modifier
-                                                    .fillMaxWidth(.9f)
-                                                    .padding(8.dp)
-                                            ,
-                                            value = query,
-                                            onValueChange = {
-                                                viewModel.onQueryChanged(it)
-                                            },
-                                            label = {
-                                                Text(text = "Search")
-                                            },
-                                            keyboardOptions = KeyboardOptions(
-                                                    keyboardType = KeyboardType.Text,
-                                                    imeAction = ImeAction.Done,
-                                            ),
-                                            leadingIcon = {
-                                                Icon(Icons.Filled.Search)
-                                            },
-                                            onImeActionPerformed = { action, softKeyboardController ->
-                                                if (action == ImeAction.Done) {
-                                                    viewModel.newSearch(query)
-                                                    softKeyboardController?.hideSoftwareKeyboard()
-                                                }
-                                            },
-                                            textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                            backgroundColor = MaterialTheme.colors.surface
-                                    )
-                                }
-                            }
-                    )
+                    ){
+                        Row(modifier = Modifier.fillMaxWidth()){
+                            TextField(
+                                    modifier = Modifier
+                                            .fillMaxWidth(.9f)
+                                            .padding(8.dp)
+                                    ,
+                                    value = query,
+                                    onValueChange = {
+                                        viewModel.onQueryChanged(it)
+                                    },
+                                    label = {
+                                        Text(text = "Search")
+                                    },
+                                    keyboardOptions = KeyboardOptions(
+                                            keyboardType = KeyboardType.Text,
+                                            imeAction = ImeAction.Done,
+                                    ),
+                                    leadingIcon = {
+                                        Icon(Icons.Filled.Search)
+                                    },
+                                    onImeActionPerformed = { action, softKeyboardController ->
+                                        if (action == ImeAction.Done) {
+                                            viewModel.newSearch(query)
+                                            softKeyboardController?.hideSoftwareKeyboard()
+                                        }
+                                    },
+                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                                    backgroundColor = MaterialTheme.colors.surface
+                            )
+                        }
+                    }
+
                     LazyColumnForIndexed(
                             items = recipes
                     ) { index, recipe ->
