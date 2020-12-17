51bb52c773fb867d9d8fcceb94227acef210dd88
bandhan <pallavi9086579857@gmail.com>
2020-12-18 00:53:48 +0530

    this is my  first testing

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //hi i am bandhan singh
     }
 }
 
