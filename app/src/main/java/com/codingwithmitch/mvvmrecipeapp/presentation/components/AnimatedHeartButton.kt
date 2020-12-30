141a9fff833984d67091ba10e545ff64795e89d3
mitch <mitch@tabian.ca>
2020-12-30 14:11:05 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/components/AnimatedHeartButton.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/components/AnimatedHeartButton.kt
@@ -11,9 +11,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
@@ -128,6 +125,5 @@ private fun HeartButton(
                     .height(state[size]),
             )
         }
-
     }
 }
