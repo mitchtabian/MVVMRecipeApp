9563ac2c9950d68a8f9150d94dcc2d0423107235
mitch <mitch@tabian.ca>
2021-01-05 12:55:56 -0800

    building a snackbar demo

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -15,12 +13,15 @@ import androidx.compose.material.icons.filled.AccountBalanceWallet
 import androidx.compose.material.icons.filled.BrokenImage
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.codingwithmitch.mvvmrecipeapp.presentation.BaseApplication
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.CircularIndeterminateProgressBar
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.LoadingRecipeListShimmer
@@ -28,7 +29,11 @@ import com.codingwithmitch.mvvmrecipeapp.presentation.components.RecipeCard
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.SearchAppBar
 import com.codingwithmitch.mvvmrecipeapp.presentation.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -62,6 +67,8 @@ class RecipeListFragment : Fragment() {
 
                     val loading = viewModel.loading.value
 
+                    val show = remember{ mutableStateOf(false)}
+                    
                     Scaffold(
                         topBar = {
                             SearchAppBar(
@@ -74,16 +81,11 @@ class RecipeListFragment : Fragment() {
                                 scrollPosition = categoryScrollPosition,
                                 onChangeScrollPosition = viewModel::onChangeCategoryScrollPosition,
                                 onToggleTheme = {
-                                    application.toggleLightTheme()
+                                    show.value = true
+//                                    application.toggleLightTheme()
                                 }
                             )
                         },
-                        bottomBar = {
-                            MyBottomBar()
-                        },
-                        drawerContent = {
-                            MyDrawer()
-                        }
                     ) {
                         Box(modifier = Modifier
                             .background(color = MaterialTheme.colors.surface)
@@ -102,49 +104,32 @@ class RecipeListFragment : Fragment() {
                             CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
                         }
 
+                        ConstraintLayout(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            val snackbar = createRef()
+                            if(show.value){
+                                lifecycleScope.launch {
+                                    delay(1000)
+                                    show.value = false
+                                }
+                                Snackbar(
+                                    modifier = Modifier.constrainAs(snackbar){
+                                        bottom.linkTo(parent.bottom)
+                                        start.linkTo(parent.start)
+                                        end.linkTo(parent.end)
+                                    }
+                                ) {
+                                    Text(text = "Hey look a snackbar")
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
 }
-
-@Composable
-fun MyBottomBar(){
-    BottomNavigation(
-        elevation = 12.dp
-    ) {
-        BottomNavigationItem(
-            icon = {Icon(Icons.Default.BrokenImage)},
-            selected = false,
-            onClick = {}
-        )
-        BottomNavigationItem(
-            icon = {Icon(Icons.Default.Search)},
-            selected = true,
-            onClick = {}
-        )
-        BottomNavigationItem(
-            icon = {Icon(Icons.Default.AccountBalanceWallet)},
-            selected = false,
-            onClick = {}
-        )
-    }
-}
-
-
-@Composable
-fun MyDrawer(){
-    Column() {
-        Text(text = "Item1")
-        Text(text = "Item2")
-        Text(text = "Item3")
-        Text(text = "Item4")
-        Text(text = "Item5")
-    }
-}
-
-
 
 
 
