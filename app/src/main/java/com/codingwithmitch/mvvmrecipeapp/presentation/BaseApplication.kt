b3c63acc7046f82f20bb75c85770c6ab05c12952
mitch <mitch@tabian.ca>
2021-01-05 11:12:33 -0800

    cleanup

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/BaseApplication.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/BaseApplication.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class BaseApplication : Application(){
 
+    // should be saved in data store
     val isDark = mutableStateOf(false)
 
     fun toggleLightTheme(){
