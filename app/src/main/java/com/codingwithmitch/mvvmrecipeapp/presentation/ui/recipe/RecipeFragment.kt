438925f15570700e65200c14bb2683c8934cefad
mitch <mitch@tabian.ca>
2021-01-12 13:35:02 -0800

    Update to include snackbar stuff

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe/RecipeFragment.kt
@@ -4,30 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.codingwithmitch.mvvmrecipeapp.presentation.BaseApplication
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.CircularIndeterminateProgressBar
+import com.codingwithmitch.mvvmrecipeapp.presentation.components.DefaultSnackbar
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.LoadingRecipeShimmer
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.RecipeView
+import com.codingwithmitch.mvvmrecipeapp.presentation.components.util.SnackbarController
 import com.codingwithmitch.mvvmrecipeapp.presentation.theme.AppTheme
 import com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe.RecipeEvent.GetRecipeEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 const val IMAGE_HEIGHT = 260
 
+@ExperimentalMaterialApi
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class RecipeFragment: Fragment() {
 
     @Inject
     lateinit var application: BaseApplication
+
+    private val snackbarController = SnackbarController(lifecycleScope)
 
     private val viewModel: RecipeViewModel by viewModels()
 
@@ -50,24 +64,46 @@ class RecipeFragment: Fragment() {
 
                 val recipe = viewModel.recipe.value
 
+                val scaffoldState = rememberScaffoldState()
+
                 AppTheme(
                     darkTheme = application.isDark.value,
                 ){
-                    val scaffoldState = rememberScaffoldState()
-
                     Scaffold(
                         scaffoldState = scaffoldState,
                         snackbarHost = {
                             scaffoldState.snackbarHostState
                         }
                     ) {
-                        if (loading && recipe == null) LoadingRecipeShimmer(imageHeight = IMAGE_HEIGHT.dp)
-                        else recipe?.let {
-                            RecipeView(
-                                recipe = it,
+                        Box (
+                            modifier = Modifier.fillMaxSize()
+                        ){
+                            if (loading && recipe == null) LoadingRecipeShimmer(imageHeight = IMAGE_HEIGHT.dp)
+                            else recipe?.let {
+                                if(it.id == 1) {
+                                    snackbarController.getScope().launch {
+                                        snackbarController.showSnackbar(
+                                            scaffoldState = scaffoldState,
+                                            message = "An error occurred with this recipe",
+                                            actionLabel = "Ok"
+                                        )
+                                    }
+                                }
+                                else{
+                                    RecipeView(
+                                        recipe = it,
+                                    )
+                                }
+                            }
+                            CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
+                            DefaultSnackbar(
+                                snackbarHostState = scaffoldState.snackbarHostState,
+                                onDismiss = {
+                                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+                                },
+                                modifier = Modifier.align(Alignment.BottomCenter)
                             )
                         }
-                        CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
                     }
                 }
             }
