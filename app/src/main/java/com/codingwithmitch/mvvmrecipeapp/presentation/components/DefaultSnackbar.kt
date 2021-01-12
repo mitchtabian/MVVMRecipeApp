51f66b48e37c54e842204dca1ec586aeee176c2a
mitch <mitch@tabian.ca>
2021-01-12 13:43:25 -0800

    update master

---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/components/DefaultSnackbar.kt
@@ -0,0 +1,56 @@
+package com.codingwithmitch.mvvmrecipeapp.presentation.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@ExperimentalMaterialApi
+@Composable
+fun DefaultSnackbar(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit?
+) {
+    SnackbarHost(
+        hostState = snackbarHostState,
+        snackbar = { data ->
+            Snackbar(
+                modifier = Modifier.padding(16.dp),
+                text = {
+                    Text(
+                        text = data.message,
+                        style = MaterialTheme.typography.body2,
+                        color = Color.White
+                    )
+                },
+                action = {
+                    data.actionLabel?.let { actionLabel ->
+                        TextButton(
+                            onClick = {
+                                onDismiss()
+                            }
+                        ) {
+                            Text(
+                                text = actionLabel,
+                                style = MaterialTheme.typography.body2,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+            )
+        },
+        modifier = modifier
+    )
+}
+
+
+
+
+
+
+
+
