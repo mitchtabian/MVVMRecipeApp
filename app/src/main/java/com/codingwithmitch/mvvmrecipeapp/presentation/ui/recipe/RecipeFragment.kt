4a8a6e3947b58b571de03b422335cf6d1aa856f1
mitch <mitch@tabian.ca>
2021-01-12 12:42:36 -0800

    revert

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/presentation/ui/recipe/RecipeFragment.kt
@@ -1,26 +1,21 @@
-package com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codingwithmitch.mvvmrecipeapp.presentation.BaseApplication
-import com.codingwithmitch.mvvmrecipeapp.presentation.components.CircularIndeterminateProgressBar
-import com.codingwithmitch.mvvmrecipeapp.presentation.components.LoadingRecipeShimmer
 import com.codingwithmitch.mvvmrecipeapp.presentation.components.RecipeView
 import com.codingwithmitch.mvvmrecipeapp.presentation.theme.AppTheme
 import com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe.RecipeEvent.GetRecipeEvent
+import com.codingwithmitch.mvvmrecipeapp.presentation.ui.recipe.RecipeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
-
-const val IMAGE_HEIGHT = 260
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -61,25 +56,15 @@ class RecipeFragment: Fragment() {
                             scaffoldState.snackbarHostState
                         }
                     ) {
-                        if (loading && recipe == null) LoadingRecipeShimmer(imageHeight = IMAGE_HEIGHT.dp)
+                        if (loading && recipe == null) Text(text = "Loading...")
                         else recipe?.let {
                             RecipeView(
                                 recipe = it,
                             )
                         }
-                        CircularIndeterminateProgressBar(isDisplayed = loading, verticalBias = 0.3f)
                     }
                 }
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
